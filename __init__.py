b182fcd60b35e838e2ae62e82de21ba18cfb79f7
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-01-08 02:20:10 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,11 @@ from math import floor
 def sleep(secs):
     if secs == 0:
         return
-    maxrange = 100 * secs
+    maxrange = 50 * secs
     if isinstance(maxrange, float):
         sleeplittle = floor(maxrange)
-        sleep_((maxrange - sleeplittle) / 100)
+        sleep_((maxrange - sleeplittle) / 50)
         maxrange = int(sleeplittle)
     if maxrange > 0:
         for _ in range(maxrange):
-            sleep_(0.008)
+            sleep_(0.016)
